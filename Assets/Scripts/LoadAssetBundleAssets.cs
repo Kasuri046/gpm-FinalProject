123e81b3af3b469e93c4741796bfeef13a5dddc8
Kasuri046 <kasurihammad@gmail.com>
2024-05-16 13:19:29 -0700

    hammad

---
+++ Assets/Scripts/LoadAssetBundleAssets.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadAssetBundleAssets : MonoBehaviour
+{
+    public string bundleName = "assetbundlebox";
+    public string assetName = "Cube";
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        AssetBundleCreateRequest asyncBundleRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, bundleName));
+        yield return asyncBundleRequest;
+
+        AssetBundle localAssetBundle = asyncBundleRequest.assetBundle;
+
+        if (localAssetBundle == null) {
+            Debug.LogError("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        AssetBundleRequest assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(assetName);
+        yield return assetRequest;
+
+        GameObject prefab = assetRequest.asset as GameObject;
+        Instantiate(prefab);
+
+        localAssetBundle.Unload(false);
+    }
+
+
+}
